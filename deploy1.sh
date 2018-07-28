d58975f87508d8d4baced36142fa54dcda3e333f
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-07-28 10:21:50 -0700

    modify war sbibank

--- deploy1.sh
+++ deploy1.sh
@@ -5,7 +5,7 @@ if [ $ENVIRONMENT = "QA" ]; then
     do
       echo "deploye war file into $i "
       sleep 3
-      sshpass -p "1234" scp target/ola.war siva@$i:/home/siva/soft/apache-tomcat-8.5.32/webapps
+      sshpass -p "1234" scp target/sbibank.war siva@$i:/home/siva/soft/apache-tomcat-8.5.32/webapps
 
       echo "starting tomcat server $i"
       sshpass -p "1234" ssh siva@$i "JAVA_HOME=/home/siva/soft/jdk1.8.0_171" "/home/siva/soft/apache-tomcat-8.5.32/bin/./startup.sh"
