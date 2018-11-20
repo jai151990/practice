16b7d538446f9c6dabbec1c39568c97ccdb12fce
jai151990 <41561661+jai151990@users.noreply.github.com>
2018-11-20 01:30:06 -0800

    Update creeate-dep.sh

--- creeate-dep.sh
+++ creeate-dep.sh
@@ -15,7 +15,7 @@ docker inspect -f '{{.NetworkSettings.Networks.bridge.IPAddress}}' `docker ps -q
 
 ################
 echo "server ssh starting"
-docker attach ford$i -it /bin/bash
+docker attach ford$i 
 #service ssh start
 for j in `cat ip.txt`
     do
